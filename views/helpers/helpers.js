12b7f3f7ba3af7a6350a60bace28f32ca4d07237
caemci <carlosecimino@gmail.com>
2020-09-26 16:19:37 -0300

    Lista de autores

--- views/helpers/helpers.js
+++ views/helpers/helpers.js
@@ -1,7 +1,12 @@
 var hbs = require('hbs');
+var moment = require('moment')
 
 hbs.registerHelper('bookinstanceStyleStatus', status => {
 	return 'text-' + (status == 'Available' ? 'success' : (status == 'Maintenance' ? 'danger' : 'warning'))
 })
 
 hbs.registerHelper('isntAvailable', status => status != 'Available')
+
+hbs.registerHelper('dateFormatted', (date) => {
+	return date ? moment(date).format('YYYY-MM-DD') : '';
+})
