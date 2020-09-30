9a8c915d75313de9d06d4cfd5717b858fbd14f76
caemci <carlosecimino@gmail.com>
2020-09-30 18:18:34 -0300

    Revisado form de autor

--- views/helpers/helpers.js
+++ views/helpers/helpers.js
@@ -12,3 +12,7 @@ hbs.registerHelper('isLastItem', (index, length) => index < length - 1);
 hbs.registerHelper('dateFormatted', (date) => {
 	return date ? moment(date).format('YYYY-MM-DD') : '';
 })
+
+hbs.registerHelper('dateFormattedFromForm', (date) => {
+	return date ? moment(date).add(1, 'days').format('YYYY-MM-DD') : '';
+})
